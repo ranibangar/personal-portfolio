c1c8f9ddac4ca5eabc4157760b7436e9c2620d57
ranibangar <ranibangar111@gmail.com>
2023-03-22 14:17:47 +0530

    new one

---
+++ script.js
@@ -0,0 +1,18 @@
+// Header toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+  this.classList.toggle('fa-xmark')
+})
+
+// Typing effect
+
+let typed = new Typed('.auto-input',{
+ strings: [ 'Front End Developer!' ,'UI Designer!' ],
+ typedSpeed:100,
+ backSpeed:100,
+ backDelay:2000,
+ loop:true,
+})
